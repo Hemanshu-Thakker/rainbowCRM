dd1062ddb261a90615373a2f0566e070f76d7303
Hemanshu Thakker <hemanshu@Hemanshus-MacBook-Pro.local>
2022-08-22 10:23:54 +0530

    JS call issue fixed

--- app/javascript/packs/ajax_calls.js
+++ app/javascript/packs/ajax_calls.js
@@ -1,34 +1,3 @@
 import $ from 'jquery';
 
-$('#popup-customer-submit').on('click', function(){
-    var form = $("#cusomer-form-id");
-    var data = {"customer": {}}
-    $.each(form.serializeArray(), function(i, field) {
-        data["customer"][field.name] = field.value;
-    });
-    data["customer"]["remote"] = true;
-    $('#customer-close').click();
-    if (true) {
-      $.ajax({
-        url: "/customers",
-        type: "POST",
-        beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-        data: data,
-        success: function(data){
-            console.log(data);
-            var option = {
-              id: data.id,
-              name: data.name + '-' + data.company_name,
-            }
-            var newOption = new Option(option.name, option.id, true, true);
-            $('#customer_select_2').append(newOption).trigger('change');
-        },
-        error: function(){
-          console.log("AJAX call unsuccesful");
-        }
-      });
-    }
-    else{
-      alert("The form has errors, couldn't be submitted");
-    }
-});
+
