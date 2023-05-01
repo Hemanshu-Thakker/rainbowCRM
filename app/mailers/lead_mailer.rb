ed2e613f880c539f4a833a11cdcc3722117e926d
Hemanshu-Thakker <hemanshupthakker@gmail.com>
2023-05-01 10:40:09 +0530

    Added email address with name

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -1,6 +1,6 @@
 class LeadMailer < ApplicationMailer
     def confirmation_mail
         @customer = params[:customer]
-        mail(to: @customer.email, subject: 'Request Received')
+        mail(to: email_address_with_name(@customer.email, @customer.name), subject: 'Request Received')
     end
 end
